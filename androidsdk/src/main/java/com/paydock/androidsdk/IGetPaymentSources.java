3c2fbabc3ca5a8d8505b56da0ba80ca7b0d1d1d8
markcardamis <markncardamis@gmail.com>
2017-08-28 15:59:35 +1000

    Change repositories

--- androidsdk/src/main/java/com/paydock/androidsdk/IGetPaymentSources.java
+++
@@ -1,7 +0,0 @@
-package com.paydock.androidsdk;
-
-import com.paydock.javasdk.Models.CustomerPaymentSourceSearchResponse;
-
-public interface IGetPaymentSources {
-    void paymentSourcesCallback(CustomerPaymentSourceSearchResponse output);
-}
--- javasdk/src/main/java/com/paydock/javasdk/Services/ITokens.java
+++
@@ -1,9 +0,0 @@
-package com.paydock.javasdk.Services;
-
-import com.paydock.javasdk.Models.TokenRequest;
-import com.paydock.javasdk.Models.TokenResponse;
-
-public interface ITokens {
-
-    TokenResponse create(TokenRequest request) throws Exception;
-}
