9949a3884724351b21b2ffb635f304d7277d3f39
markcardamis <markncardamis@gmail.com>
2017-09-21 14:02:45 +1000

    Fixed absolute path used for webhook test transaction

--- javasdk/src/test/java/com/paydock/javasdk/Services/WebhooksTest.java
+++ javasdk/src/test/java/com/paydock/javasdk/Services/WebhooksTest.java
@@ -1,6 +1,7 @@
 package com.paydock.javasdk.Services;
 
 import com.paydock.javasdk.Models.WebhookCardExpiration;
+import com.paydock.javasdk.Models.WebhookSubscriptionEvents;
 import com.paydock.javasdk.Models.WebhookSubscriptionTransaction;
 import com.paydock.javasdk.Models.WebhookTransaction;
 
@@ -19,21 +20,21 @@ public class WebhooksTest {
 
     @Test
     public void transaction_success() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_success.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_success.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("transaction_success", webhookTransaction.event);
     }
 
     @Test
     public void transaction_failure() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_failure.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_failure.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("transaction_failure", webhookTransaction.event);
     }
 
     @Test
     public void transaction_subscription_success() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_subscription_success.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_subscription_success.json");
         WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
                 ParseSubscriptionTransaction(postData);
         Assert.assertEquals("subscription_transaction", webhooksubscriptionTransaction.event);
@@ -41,7 +42,7 @@ public class WebhooksTest {
 
     @Test
     public void transaction_subscription_failure() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_subscription_failure.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_subscription_failure.json");
         WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
                 ParseSubscriptionTransaction(postData);
         Assert.assertEquals("subscription_transaction_failure", webhooksubscriptionTransaction.event);
@@ -49,89 +50,83 @@ public class WebhooksTest {
 
     @Test
     public void subscription_creation_success() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//subscription_creation_success.json");
-        WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
-                ParseSubscriptionTransaction(postData);
-        Assert.assertEquals("subscription_creation_success", webhooksubscriptionTransaction.event);
+        String postData = loadFile("/javasdk/Webhooks/subscription_creation_success.json");
+        WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
+                ParseSubscriptionEvent(postData);
+        Assert.assertEquals("subscription_creation_success", webhooksubscriptionEvents.event);
     }
 
     @Test
     public void subscription_creation_failure() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//subscription_creation_failure.json");
-        WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
-                ParseSubscriptionTransaction(postData);
-        Assert.assertEquals("subscription_creation_failure", webhooksubscriptionTransaction.event);
+        String postData = loadFile("/javasdk/Webhooks/subscription_creation_failure.json");
+        WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
+                ParseSubscriptionEvent(postData);
+        Assert.assertEquals("subscription_creation_failure", webhooksubscriptionEvents.event);
     }
 
     @Test
     public void subscription_finished() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//subscription_finished.json");
-        WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
-                ParseSubscriptionTransaction(postData);
-        Assert.assertEquals("subscription_finished", webhooksubscriptionTransaction.event);
+        String postData = loadFile("/javasdk/Webhooks/subscription_finished.json");
+        WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
+                ParseSubscriptionEvent(postData);
+        Assert.assertEquals("subscription_finished", webhooksubscriptionEvents.event);
     }
 
     @Test
     public void subscription_updated() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//subscription_updated.json");
-        WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
-                ParseSubscriptionTransaction(postData);
-        Assert.assertEquals("subscription_updated", webhooksubscriptionTransaction.event);
+        String postData = loadFile("/javasdk/Webhooks/subscription_updated.json");
+        WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
+                ParseSubscriptionEvent(postData);
+        Assert.assertEquals("subscription_updated", webhooksubscriptionEvents.event);
     }
 
     @Test
     public void subscription_failed() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//subscription_failed.json");
-        WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
-                ParseSubscriptionTransaction(postData);
-        Assert.assertEquals("subscription_failed", webhooksubscriptionTransaction.event);
+        String postData = loadFile("/javasdk/Webhooks/subscription_failed.json");
+        WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
+                ParseSubscriptionEvent(postData);
+        Assert.assertEquals("subscription_failed", webhooksubscriptionEvents.event);
     }
 
     @Test
     public void transaction_refund_request() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_refund_request.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_refund_request.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_requested", webhookTransaction.event);
     }
 
     @Test
     public void transaction_refund_success() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_refund_success.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_refund_success.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_success", webhookTransaction.event);
     }
 
     @Test
     public void transaction_refund_failure() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//transaction_refund_failure.json");
+        String postData = loadFile("/javasdk/Webhooks/transaction_refund_failure.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_failure", webhookTransaction.event);
     }
 
     @Test
     public void card_expiration_warning() throws Exception {
-        String postData = loadFile("javasdk//Webhooks//card_expiration_warning.json");
+        String postData = loadFile("/javasdk/Webhooks/card_expiration_warning.json");
         WebhookCardExpiration webhookCardExpiration = new Webhooks().ParseCardExpiry(postData);
         Assert.assertEquals("card_expiration_warning", webhookCardExpiration.event);
     }
 
-    private String loadFile(String path){
-        try{
-            File file = new File(path);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader Buffreader = new BufferedReader(fileReader);
-            StringBuilder response = new StringBuilder();
-            String line;
-            while((line = Buffreader.readLine()) != null){
-                response.append(line).append('\n');
-            }
-            line = response.toString();
-            return line;
-
-        }catch(Exception ex){
-            ex.printStackTrace();
+    private String loadFile(String path) throws Exception {
+        String root = System.getProperty("user.dir");
+        File file = new File(root + path);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader Buffreader = new BufferedReader(fileReader);
+        StringBuilder response = new StringBuilder();
+        String line;
+        while((line = Buffreader.readLine()) != null){
+            response.append(line).append('\n');
         }
-        return null;
+        line = response.toString();
+        return line;
     }
-
 }
