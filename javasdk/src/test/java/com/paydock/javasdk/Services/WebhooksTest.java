bae572bcf2151642c598c934dcf44c24d848ed72
markcardamis <markncardamis@gmail.com>
2017-09-21 14:53:37 +1000

    Changed location on webhook sample data

--- javasdk/src/test/java/com/paydock/javasdk/Services/WebhooksTest.java
+++ javasdk/src/test/java/com/paydock/javasdk/Services/WebhooksTest.java
@@ -20,21 +20,21 @@ public class WebhooksTest {
 
     @Test
     public void transaction_success() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_success.json");
+        String postData = loadFile("transaction_success.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("transaction_success", webhookTransaction.event);
     }
 
     @Test
     public void transaction_failure() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_failure.json");
+        String postData = loadFile("transaction_failure.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("transaction_failure", webhookTransaction.event);
     }
 
     @Test
     public void transaction_subscription_success() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_subscription_success.json");
+        String postData = loadFile("transaction_subscription_success.json");
         WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
                 ParseSubscriptionTransaction(postData);
         Assert.assertEquals("subscription_transaction", webhooksubscriptionTransaction.event);
@@ -42,7 +42,7 @@ public class WebhooksTest {
 
     @Test
     public void transaction_subscription_failure() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_subscription_failure.json");
+        String postData = loadFile("transaction_subscription_failure.json");
         WebhookSubscriptionTransaction webhooksubscriptionTransaction = new Webhooks().
                 ParseSubscriptionTransaction(postData);
         Assert.assertEquals("subscription_transaction_failure", webhooksubscriptionTransaction.event);
@@ -50,7 +50,7 @@ public class WebhooksTest {
 
     @Test
     public void subscription_creation_success() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/subscription_creation_success.json");
+        String postData = loadFile("subscription_creation_success.json");
         WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
                 ParseSubscriptionEvent(postData);
         Assert.assertEquals("subscription_creation_success", webhooksubscriptionEvents.event);
@@ -58,7 +58,7 @@ public class WebhooksTest {
 
     @Test
     public void subscription_creation_failure() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/subscription_creation_failure.json");
+        String postData = loadFile("subscription_creation_failure.json");
         WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
                 ParseSubscriptionEvent(postData);
         Assert.assertEquals("subscription_creation_failure", webhooksubscriptionEvents.event);
@@ -66,7 +66,7 @@ public class WebhooksTest {
 
     @Test
     public void subscription_finished() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/subscription_finished.json");
+        String postData = loadFile("subscription_finished.json");
         WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
                 ParseSubscriptionEvent(postData);
         Assert.assertEquals("subscription_finished", webhooksubscriptionEvents.event);
@@ -74,7 +74,7 @@ public class WebhooksTest {
 
     @Test
     public void subscription_updated() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/subscription_updated.json");
+        String postData = loadFile("subscription_updated.json");
         WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
                 ParseSubscriptionEvent(postData);
         Assert.assertEquals("subscription_updated", webhooksubscriptionEvents.event);
@@ -82,7 +82,7 @@ public class WebhooksTest {
 
     @Test
     public void subscription_failed() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/subscription_failed.json");
+        String postData = loadFile("subscription_failed.json");
         WebhookSubscriptionEvents webhooksubscriptionEvents = new Webhooks().
                 ParseSubscriptionEvent(postData);
         Assert.assertEquals("subscription_failed", webhooksubscriptionEvents.event);
@@ -90,35 +90,35 @@ public class WebhooksTest {
 
     @Test
     public void transaction_refund_request() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_refund_request.json");
+        String postData = loadFile("transaction_refund_request.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_requested", webhookTransaction.event);
     }
 
     @Test
     public void transaction_refund_success() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_refund_success.json");
+        String postData = loadFile("transaction_refund_success.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_success", webhookTransaction.event);
     }
 
     @Test
     public void transaction_refund_failure() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/transaction_refund_failure.json");
+        String postData = loadFile("transaction_refund_failure.json");
         WebhookTransaction webhookTransaction = new Webhooks().ParseTransaction(postData);
         Assert.assertEquals("refund_failure", webhookTransaction.event);
     }
 
     @Test
     public void card_expiration_warning() throws Exception {
-        String postData = loadFile("/javasdk/Webhooks/card_expiration_warning.json");
+        String postData = loadFile("card_expiration_warning.json");
         WebhookCardExpiration webhookCardExpiration = new Webhooks().ParseCardExpiry(postData);
         Assert.assertEquals("card_expiration_warning", webhookCardExpiration.event);
     }
 
     private String loadFile(String path) throws Exception {
         String root = System.getProperty("user.dir");
-        File file = new File(root + path);
+        File file = new File(root + "/src/test/java/com/paydock/javasdk/WebhookSamples/" + path);
         FileReader fileReader = new FileReader(file);
         BufferedReader Buffreader = new BufferedReader(fileReader);
         StringBuilder response = new StringBuilder();
