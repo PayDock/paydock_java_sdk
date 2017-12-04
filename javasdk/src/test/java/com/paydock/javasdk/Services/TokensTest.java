d1239ba2d8e159a8c1f44b950784f63c4222ebd5
markcardamis <markncardamis@gmail.com>
2017-12-04 15:47:12 +1100

    Added charge with token functional test

--- javasdk/src/test/java/com/paydock/javasdk/Services/TokensTest.java
+++ javasdk/src/test/java/com/paydock/javasdk/Services/TokensTest.java
@@ -1,5 +1,7 @@
 package com.paydock.javasdk.Services;
 
+import com.paydock.javasdk.Models.ChargeRequest;
+import com.paydock.javasdk.Models.ChargeResponse;
 import com.paydock.javasdk.Models.TokenRequest;
 import com.paydock.javasdk.Models.TokenResponse;
 import com.paydock.javasdk.PayDock;
@@ -9,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.math.BigDecimal;
 
 @RunWith(JUnit4.class)
 public class TokensTest {
@@ -30,10 +34,28 @@ public class TokensTest {
         return new Tokens().create(tokenRequest);
     }
 
+    private ChargeResponse CreateChargeWithToken(BigDecimal chargeAmount,
+                                             String paymentToken) throws Exception {
+        ChargeRequest charge = new ChargeRequest();
+        charge.currency = "AUD";
+        charge.description = "Test charge description";
+        charge.amount = chargeAmount;
+        charge.token = paymentToken;
+        return new Charges().add(charge);
+    }
+
     @Test
     public void create() throws Exception {
         TokenResponse token = createToken();
         Assert.assertTrue(token.get_IsSuccess());
     }
+
+    @Test
+    public void createCharge() throws Exception {
+        TokenResponse token = createToken();
+        ChargeResponse result = CreateChargeWithToken(new BigDecimal(8), token.resource.data);
+        Assert.assertTrue(result.get_IsSuccess());
+    }
+
 
 }
