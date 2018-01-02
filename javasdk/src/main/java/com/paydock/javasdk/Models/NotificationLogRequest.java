052e0aae0c0bb793bcc8fbfb7b0eb041b9b43376
markcardamis <markncardamis@gmail.com>
2018-01-02 16:57:56 +1100

    Changed the variable in the models for notifications from 'eventTrigger' to 'event' Added missing notification endpoint methods

--- javasdk/src/main/java/com/paydock/javasdk/Models/NotificationLogRequest.java
+++ javasdk/src/main/java/com/paydock/javasdk/Models/NotificationLogRequest.java
@@ -7,12 +7,12 @@ public class NotificationLogRequest
 {
     public String _id;
     public Boolean success;
-    public String eventTrigger;
+    public String event;
     public String type;
     public Date created_at_from = null;
     public Date created_at_to = null;
     public String parent_id;
     public String destination;
-    }
+}
 
 
